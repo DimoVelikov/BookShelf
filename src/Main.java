83005899f1d544a3364befa307efb9bb627ef00b
Dimo <dvelikov24@gmail.com>
2022-02-09 09:48:28 -0500

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+
+        //Test 1
+        // user input String "jAVa" ----> expected result "Java"
+
+        System.out.println(Book.capitalize("jAVa"));
+        // test passed!
+
+        //Test 2
+        // capitalize("HTML") => "Html"
+
+        System.out.println(Book.capitalize("HTML"));
+        //Test passed!
+
+    }
 }
