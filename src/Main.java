9f126087790ddb676788fbaa5d960e2644e443e3
Dimo <dvelikov24@gmail.com>
2022-02-15 10:50:45 -0500

    Testing findBookByPartialTitle

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Test 1
-        // user input String "jAVa" ----> expected result "Java"
+        //Testing findBookByPartialTitle
 
-        System.out.println(Book.capitalize("jAVa"));
-        // test passed!
 
-        //Test 2
-        // capitalize("HTML") => "Html"
+        //Creating a Book Object with addBook
+        Shelf.addBook(12,"Dimo Velikov","Java is hard",100);
+        //Confirm that Book object is created
+        System.out.println(Shelf.getTheBooks());
+        //Expected result:[[author=Dimo Velikov, title=Java, id=12, page=100]]
 
-        System.out.println(Book.capitalize("HTML"));
-        //Test passed!
+        //Testing if findingBookByPartialTitle is working
+        System.out.println(Shelf.findBookByPartialTitle("Java"));
+        //Expected result [author=Dimo Velikov, title=Java is hard, id=12, page=100]
+
+        //Test completed! TEST PASS!!!
+
+
 
     }
 }
