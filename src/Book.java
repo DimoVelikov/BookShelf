b4ef4486dc60d4e276e4a6aa435ab1c8533e3951
LenaPC <elenachubukova@yahoo.com>
2022-02-16 09:14:18 -0500

    new codes book (isTitleWord)

--- src/Book.java
+++ src/Book.java
@@ -55,33 +55,20 @@ public class Book {
      */
     public static Boolean isTitleWord(String word) {
 
-        if (word.equalsIgnoreCase("a") ||
-                word.equalsIgnoreCase("an") ||
-                word.equalsIgnoreCase("the") ||
-                word.equalsIgnoreCase("and") ||
-                word.equalsIgnoreCase("but") ||
-                word.equalsIgnoreCase("for") ||
-                word.equalsIgnoreCase("nor") ||
-                word.equalsIgnoreCase("or") ||
-                word.equalsIgnoreCase("so") ||
-                word.equalsIgnoreCase("yet") ||
-                word.equalsIgnoreCase("at") ||
-                word.equalsIgnoreCase("by") ||
-                word.equalsIgnoreCase("in") ||
-                word.equalsIgnoreCase("into") ||
-                word.equalsIgnoreCase("near") ||
-                word.equalsIgnoreCase("of") ||
-                word.equalsIgnoreCase("on") ||
-                word.equalsIgnoreCase("to") ||
-                word.equalsIgnoreCase("than") ||
-                word.equalsIgnoreCase("via")) {
-            return false;
-        } else {
-            return true;
+        boolean result=true;
+        String[] nonTitle={"a" , "an", "the","and", "but", "for", "nor", "or", "so", "yet","at", "by", "in", "into", "near", "of", "on", "to", "than", "via"};
+
+        for (String each : nonTitle) {
+            if(word.equals(each)){
+                result=false;
+                break;
+            }
         }
 
+        return result;
 
     }
+
 
     /**
      * No args constructor. Stays empty
