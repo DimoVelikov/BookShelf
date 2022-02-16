e3dc5b4c6765c178c06c45e461ab9a6ba415b9bc
LenaPC <elenachubukova@yahoo.com>
2022-02-16 10:52:16 -0500

    TC007

--- src/Book.java
+++ src/Book.java
@@ -65,7 +65,7 @@ public class Book {
     public static Boolean isTitleWord(String word) {
 
         boolean result=true;
-        String[] nonTitle={"a" , "an", "the","and", "but", "for", "nor", "or", "so", "yet","at", "by", "in", "into", "near", "of", "on", "to", "than", "via"};
+        String[] nonTitle={"a" , "What is ASCII code?", "an", "the","and", "but", "for", "nor", "or", "so", "yet","at", "by", "in", "into", "near", "of", "on", "to", "than", "via"};
 
         for (String each : nonTitle) {
             if(word.equals(each)){
